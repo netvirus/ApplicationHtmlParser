19e9cadb983c09bbb51b6cddabfc0a76b79f10f5
yaroslav-tsuprak <yaroslav.tsuprak@gmail.com>
2021-02-03 00:02:29 +0700

    Added list of files

--- src/main/java/org/netvirus/ApplicationHtmlParser.java
+++ src/main/java/org/netvirus/ApplicationHtmlParser.java
@@ -3,8 +3,14 @@ package org.netvirus;
 import org.netvirus.data.HtmlParser;
 import org.netvirus.model.HtmlFile;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.SQLOutput;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ApplicationHtmlParser {
@@ -12,7 +18,35 @@ public class ApplicationHtmlParser {
     public static void main(String[] args) throws IOException {
 
         HtmlParser htmlParser = HtmlParser.getInstance();
-        HtmlFile htmlFile = htmlParser.load("test.htm");
-        System.out.println(htmlFile.getFileText());
+//        File jarDir = new File(ClassLoader.getSystemClassLoader().getResource(".").getPath());
+//        System.out.println(jarDir.getAbsolutePath());
+
+        Path start = Paths.get("C:\\b\\");
+        try (Stream<Path> stream = Files.walk(start, Integer.MAX_VALUE)) {
+            List<String> collect = stream
+                    .map(String::valueOf)
+                    .sorted()
+                    .collect(Collectors.toList());
+
+            collect.forEach(fileName -> {
+                if (fileName.contains(".htm")) {
+                    HtmlFile htmlFile = new HtmlFile();
+                    try {
+                        htmlFile = htmlParser.load(fileName);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        File newFile = new File(fileName);
+                        FileWriter fileWriter = new FileWriter(newFile, false);
+                        fileWriter.write(htmlFile.getFileText().toString());
+                        fileWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
     }
 }
